7828a891618a28953a6865a4b1c6e15e00afef68
liangmiQwQ <mi_yyds@126.com>
2025-09-19 20:40:22 +0800

    feat: add progress

--- src/components/export.vine.ts
+++ src/components/export.vine.ts
@@ -1,4 +1,6 @@
+import type { StyleValue } from 'vue'
 import type { Settings } from '@/settings'
+import { computed } from 'vue'
 import { CardOption, UiCard } from '@/ui/card-element.vine'
 import { UiButton, UiSelect } from '@/ui/forms.vine'
 
@@ -15,9 +17,10 @@ export function AppExport() {
       backdrop-brightness-95
       z-panel-content
       z-1
-      flex="~ items-center justify-center"
+      flex="~ items-center justify-between col"
       @click.self="cancel"
     >
+      <div />
       <UiCard min-w-100 class="bg-white dark:bg-dark-900">
         <div flex="~ col gap-4">
           <div flex="~ items-center justify-between">
@@ -38,6 +41,28 @@ export function AppExport() {
           <UiButton class="!p1">Generate GIF</UiButton>
         </div>
       </UiCard>
+      <ExportProgress :steps="['Configuration']" />
+    </div>
+  `
+}
+
+export function ExportProgress() {
+  const steps = vineProp<string[]>()
+  const currentStep = vineProp.withDefault(0)
+
+  const style = computed((): StyleValue => ({
+    width: `${100 * (currentStep.value + 1 / steps.value.length)}%`,
+  }))
+
+  return vine`
+    <div py8 font-medium text-sm select-none flex="~ col items-center gap-2" min-w-80>
+      <div flex="~ items-center gap-2">
+        <span>{{ steps[currentStep] }}</span>
+        <span>{{ currentStep }} of {{ steps.length - 1 }}</span>
+      </div>
+      <div rounded-full w-full h-2 of-hidden op50>
+        <div :style class="h-full" duration-300 dark:bg-white bg-neutral-400 />
+      </div>
     </div>
   `
 }
