47bc0ec96c56021b7d3906424be0cf4d482c81ce
liangmiQwQ <github@liangmi.dev>
2025-12-31 22:22:11 +0800

    feat: adjust ui, introduce svg

--- src/app.vine.ts
+++ src/app.vine.ts
@@ -12,6 +12,7 @@ export function App() {
   const { exporting, cancelExport, startExport } = useExport()
 
   return vine`
+    <Footer />
     <main w-full min-h-screen transition-colors duration-300>
       <!-- Export -->
       <AppExport :settings v-if="exporting" @cancel="cancelExport" />
@@ -41,14 +42,17 @@ function AppHeader() {
 
   return vine`
     <!-- Header -->
-    <nav flex="~ justify-between items-center" w-full px-2>
-      <h1 class="text-lg" flex="~ items-center gap-1" op90>
-        <span font-medium>Simple</span>
-        <span op80>Introduce</span>
-        <a op60 text-base underline href="https://github.com/liangmiqwq" target="_blank"
-          >by Liang Mi</a
-        >
-      </h1>
+    <nav flex="~ justify-between items-center" w-full pr-2>
+      <div flex="~ items-center gap-1">
+        <UiButton px-4>
+          <div i-hugeicons-gif01 />
+          .gif
+        </UiButton>
+        <!-- <UiButton px-4 :disabled="true">
+          <div i-hugeicons-svg01 />
+          .svg
+        </UiButton> -->
+      </div>
       <div flex="~ items-center gap-1">
         <UiButton @click="() => toggleDarkMode()" title="Toggle dark mode" type="ghost">
           <div v-if="isDark" i-ph-sun-duotone />
@@ -57,21 +61,33 @@ function AppHeader() {
         <UiButton title="Github" type="ghost" href="https://github.com/liangmiQwQ/simple-introduce">
           <div class="i-mdi-github" />
         </UiButton>
-        <div flex="~ items-center gap-2" select-none ml-2>
-          <div op50>Made with</div>
-          <a
-            class="op60 hover:op70"
-            cursor-pointer
-            font-bold
-            flex="~ items-center gap-1"
-            href="https://github.com/vue-vine/vue-vine"
-            target="_blank"
+        <span op90 flex gap-1>
+          <span font-medium>Simple</span>
+          <span op80>Introduce</span>
+          <a op60 text-base underline href="https://github.com/liangmiqwq" target="_blank"
+            >by Liang Mi</a
           >
-            <img :src="VineLogo" width="20" height="20" />
-            <div flex="~">Vue-Vine</div>
-          </a>
-        </div>
+        </span>
       </div>
     </nav>
+  `
+}
+
+function Footer() {
+  return vine`
+    <div flex="~ items-center gap-2" select-none ml-2 fixed bottom-5 right-5>
+      <div op50>Made with</div>
+      <a
+        class="op60 hover:op70"
+        cursor-pointer
+        font-bold
+        flex="~ items-center gap-1"
+        href="https://github.com/vue-vine/vue-vine"
+        target="_blank"
+      >
+        <img :src="VineLogo" width="20" height="20" />
+        <div flex="~">Vue-Vine</div>
+      </a>
+    </div>
   `
 }
