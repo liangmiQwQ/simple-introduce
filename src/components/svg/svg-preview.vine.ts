0cd7db7e5631e8aad5174d595c5f17b72c7a123e
liangmiQwQ <github@liangmi.dev>
2026-01-01 22:59:47 +0800

    poc

--- src/components/svg/svg-preview.vine.ts
+++ src/components/svg/svg-preview.vine.ts
@@ -1,10 +1,20 @@
+import type { StyleValue } from 'vue'
 import type { Settings } from '../../settings'
+import { computed } from 'vue'
 
 export function SvgPreview({ settings, height, width, appearance }: {
   settings: Settings
   height?: number
   width?: number
-  appearance?: 'light' | 'dark'
 }) {
-  return vine``
+  const style = computed((): StyleValue => ({
+    height: `${height}px`,
+    width: `${width}px`,
+  }))
+
+  return vine`
+    <div :style>
+      <img src="../../assets/compile.svg" />
+    </div>
+  `
 }
